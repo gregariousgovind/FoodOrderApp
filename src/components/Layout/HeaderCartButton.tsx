b7e782a87600b3e321a180763b9af2a3a58737fb
gregariousgovind <govind.chauhan@mountblue.io>
2022-01-06 20:26:02 +0530

    1 file modified

--- src/components/Layout/HeaderCartButton.tsx
+++ src/components/Layout/HeaderCartButton.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 
 import CartIcon from '../Cart/CartIcon';
@@ -6,13 +6,34 @@ import CartIcon from '../Cart/CartIcon';
 import './HeaderCartButton.scss';
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighLighted] = useState(false);
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
   const numberOfCartItems = cartCtx.items.reduce((curentNo, item) => {
     return curentNo + item.amount;
   }, 0);
 
+  const btnClasses = btnIsHighlighted ? 'bump' : '';
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsHighLighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighLighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className="header-cart-button" onClick={props.onClick}>
+    <button
+      className={`header-cart-button ${btnClasses}`}
+      onClick={props.onClick}
+    >
       <span className="icon">
         <CartIcon />
       </span>
